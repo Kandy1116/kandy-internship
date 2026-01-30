542923874394da7af15232283cb5338ac7366f91
Kandy1116 <venus.kandy@gmail.com>
2026-01-30 09:50:30 -0600

    update work

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
 import { Link, useParams } from "react-router-dom";
-import axios from "axios";
+import axios from "../api/axios";
 import Skeleton from "../components/UI/Skeleton";
 
 const ItemDetails = () => {
